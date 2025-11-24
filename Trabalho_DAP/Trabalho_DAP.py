eb237d892a7e7455bd8f0fb6a2890664248d96cf
Aluno <aluno@alunos.ce.senac.br>
2025-11-24 20:16:01 -0300

    Trabalho DAP

---
+++ Trabalho_DAP/Trabalho_DAP.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df = pd.read_excel('C:/Users/Aluno/Documents/Tiago Pitombeira/SENAC-Python/Trabalho_DAP/Relatorio DAP.xlsx')
+
+print(df.head())
+
